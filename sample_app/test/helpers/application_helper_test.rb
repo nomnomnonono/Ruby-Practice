d0368bbde3cb7873bff270a73ecf6e2ff0761c6a
nomnomnonono <murumuru1204@gmail.com>
2023-05-19 00:10:24 +0900

    add chapter 6

--- sample_app/test/helpers/application_helper_test.rb
+++
@@ -1,8 +0,0 @@
-require "test_helper"
-
-class ApplicationHelperTest < ActionView::TestCase
-  test "full title helper" do
-    assert_equal （コードを書き込む）, full_title
-    assert_equal （コードを書き込む）, full_title("Help")
-  end
-end
